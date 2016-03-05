c747e240a239fc98bb20820371d0eb4586cb84c1
Kashav Madan <kshvmdn@gmail.com>
2016-03-05 05:41:26 -0500

    Add switch for byte size

--- client/click.template.js
+++ client/click.template.js
@@ -4,6 +4,31 @@ Template.byte.helpers({
   },
   binary: function () {
     return Session.get('score').toString(2);
+  },
+  text: function () {
+    var score = Session.get('score');
+    var conversionFactor, byteType;
+    if (score >= 1099511627776) {
+      conversionFactor = 1099511627776;
+      byteType = 'Terabyte(s)';
+    } else if (score >= 1073741824) {
+      conversionFactor = 1073741824;
+      byteType = 'Gigabyte(s)';
+    } else if (score >= 1048576) {
+      conversionFactor = 1048576;
+      byteType = 'Megabyte(s)';
+    } else if (score >= 1024) {
+      conversionFactor = 1024;
+      byteType = 'Kilobyte(s)';
+    } else {
+      conversionFactor = 1;
+      byteType = 'Byte(s)';
+    }
+
+    var num = (score / conversionFactor).toFixed(2)
+    if (num.slice(-2) == '00')
+      num = num.slice(0, -3);
+    return num + ' ' + byteType;
   }
 });
 
