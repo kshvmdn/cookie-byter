545516e5ebd43c0a61a920661c7bcf1a0eb86008
Kashav Madan <kshvmdn@gmail.com>
2016-03-04 22:44:32 -0500

    Add client

---
+++ client/click.template.js
@@ -0,0 +1,16 @@
+Template.byte.helpers({
+  score: function () {
+    return Session.get('score');
+  }
+});
+
+Template.byte.events({
+  'click button': function () {
+    if (interval === undefined) {
+      interval = Meteor.setInterval(function () {
+        Session.set('score', Session.get('score') + (1 * Session.get('rate')));
+      }, Session.get('timeInterval'));
+    }
+    Session.set('score', Session.get('score') + (1 * Session.get('rate')));
+  }
+});
