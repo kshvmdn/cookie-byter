555b6583d7879ede39fd3589fae6454df7d15a59
Kashav Madan <kshvmdn@gmail.com>
2016-03-05 06:17:16 -0500

    Byte formatting for upgrades/workers

--- client/upgrade.template.js
+++ client/upgrade.template.js
@@ -4,6 +4,29 @@ Template.upgrade.helpers({
     _.forEach(Session.get('upgrades'), function(upgrade, key) {
       upgrade.disabled = upgrade.owned ? 'disabled' : '';
       upgrade.class_ = key;
+      var p = upgrade.price;
+      var factor, byteType;
+      if (p >= 1099511627776) {
+        factor = 1099511627776;
+        byteType = 'TB';
+      } else if (p >= 1073741824) {
+        factor = 1073741824;
+        byteType = 'GB';
+      } else if (p >= 1048576) {
+        factor = 1048576;
+        byteType = 'MB';
+      } else if (p >= 1024) {
+        factor = 1024;
+        byteType = 'KB';
+      } else {
+        factor = 1;
+        byteType = 'B';
+      }
+
+      var n = (p / factor).toFixed(2);
+      if (n.slice(-2) == '00')
+        n = n.slice(0, -3);
+      upgrade.priceText = n + ' ' + byteType;
       upgrades.push(upgrade);
     });
     return upgrades;
--- client/worker.template.js
+++ client/worker.template.js
@@ -3,6 +3,28 @@ Template.worker.helpers({
     people = []
     _.forEach(Session.get('people'), function(person, key) {
       person.class_ = key;
+      var p = person.price;
+      var conversionFactor, byteType;
+      if (p >= 1099511627776) {
+        conversionFactor = 1099511627776;
+        byteType = 'TB';
+      } else if (p >= 1073741824) {
+        conversionFactor = 1073741824;
+        byteType = 'GB';
+      } else if (p >= 1048576) {
+        conversionFactor = 1048576;
+        byteType = 'MB';
+      } else if (p >= 1024) {
+        conversionFactor = 1024;
+        byteType = 'KB';
+      } else {
+        conversionFactor = 1;
+        byteType = 'B';
+      }
+      var n =  (p / conversionFactor).toFixed(2);
+      if (n.slice(-2) == '00')
+        n = n.slice(0, -3);
+      person.priceText = n + ' ' + byteType;
       people.push(person);
     });
     return people;
