07396c7b11b8fadf0f720f15c18716c045bbc287
Kashav Madan <kshvmdn@gmail.com>
2016-03-05 02:10:45 -0500

    Cleanup setPersistent

--- client/buy.template.js
+++ client/buy.template.js
@@ -3,11 +3,14 @@ Template.worker.events({
     var people = Session.get('people');
     var person = people[event.target.id];
     if (Session.get('score') >= person.price) {
-      Session.setPersistent('score', Session.get('score') - person.price);
-      Session.setPersistent('boostrate', Session.get('boostrate') + person.boostrate);
       person.numberOwned += 1;
       people[event.target.id] = person;
-      Session.setPersistent('people', people);
+
+      Session.setPersistent({
+        score: Session.get('score') - person.price,
+        boostrate: Session.get('boostrate') + person.boostrate,
+        people: people
+      });
     }
   }
 });
